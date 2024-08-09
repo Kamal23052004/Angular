52e514a837e5a845587817f6d57c0db3cfd9aa01
Kamal <158833565+Kamal23052004@users.noreply.github.com>
2024-08-09 10:37:31 +0000

    Migrating app to Module

--- first-angular-project/src/app/app.component.ts
+++ first-angular-project/src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
-
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
-import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
 })
+
 export class AppComponent {
   users = DUMMY_USERS;
   selectedUserId?: string;
@@ -25,4 +20,4 @@ export class AppComponent {
   onSelectUser(id: string) {
     this.selectedUserId = id;
   }
-}   
+}
