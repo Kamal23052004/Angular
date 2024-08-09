8b95d6e3d366f49e532eaaa2f00a81eabc1d33c2
Kamal <158833565+Kamal23052004@users.noreply.github.com>
2024-08-09 08:51:43 +0000

    final commit

--- first-angular-project/src/app/tasks/tasks.component.ts
+++ first-angular-project/src/app/tasks/tasks.component.ts
@@ -1,71 +1,34 @@
-import { Component,Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from './new-task/new-task.component';
+import { NewTaskComponent } from "./new-task/new-task.component";
 import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
+
 @Component({
-  selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, NewTaskComponent],
-  templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+    selector: 'app-tasks',
+    standalone: true,
+    templateUrl: './tasks.component.html',
+    styleUrl: './tasks.component.css',
+    imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
-  @Input({required: true}) userId!: string;
-  @Input({required: true}) name!: string;
-
-
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
   isAddingTask = false;
 
+  constructor(private tasksService: TasksService){}
+  
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31'
-    },
-    {
-      id: 't2',
-      userId: 'u2',
-      title: 'Master Angular 2',
-      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-22'
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Master Angular 3',
-      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-20'
-    },
-  ];
-  get selectedUserTasks(){
-    return this.tasks.filter((task) => task.userId === this.userId);
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
   }
-
-  onCompletetask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
-  }
-
-
-  onStartAddTask(){
+  
+  onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
-
-  onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    })
-    this.isAddingTask = false;
-  }
-}  
-
+}
