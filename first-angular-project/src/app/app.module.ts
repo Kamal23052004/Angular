27d969b32d462a5f6810f47a4bb9ed74163e0aa2
Kamal <158833565+Kamal23052004@users.noreply.github.com>
2024-08-09 12:35:00 +0000

    project completion !

--- first-angular-project/src/app/app.module.ts
+++ first-angular-project/src/app/app.module.ts
@@ -1,14 +1,11 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { CardComponent } from "./shared/card/card.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { TaskComponent } from "./tasks/task/task.component";
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 
 
@@ -17,12 +14,9 @@ import { TaskComponent } from "./tasks/task/task.component";
     declarations: [
         AppComponent, 
         HeaderComponent, 
-        UserComponent, 
-        CardComponent, 
-        TasksComponent, 
-        TaskComponent, 
-        NewTaskComponent],
+        UserComponent,
+    ],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule {}
