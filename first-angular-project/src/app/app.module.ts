1eaa73ff381db4b8f2617512c1bb71650aee1b8e
Kamal <158833565+Kamal23052004@users.noreply.github.com>
2024-08-09 11:34:19 +0000

    bug fixes

--- first-angular-project/src/app/app.module.ts
+++ first-angular-project/src/app/app.module.ts
@@ -1,26 +1,28 @@
+import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { BrowserModule } from "@angular/platform-browser";
 import { TasksComponent } from "./tasks/tasks.component";
 import { CardComponent } from "./shared/card/card.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { TaskComponent } from "./tasks/task/task.component";
-import { FormsModule } from "@angular/forms";
-
-
-
-
-
-
 
 
 
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, UserComponent, CardComponent, TasksComponent, TaskComponent, NewTaskComponent],
+    declarations: [
+        AppComponent, 
+        HeaderComponent, 
+        UserComponent, 
+        CardComponent, 
+        TasksComponent, 
+        TaskComponent, 
+        NewTaskComponent],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule]
+    imports: [BrowserModule, FormsModule],
 })
 export class AppModule {}
--- first-angular-project/src/app/shared/card/card.component.ts
+++ first-angular-project/src/app/shared/card/card.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  imports: [],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
-
-}
+export class CardComponent {}
