9b1e7af329d06aad700faf893b6330db4d18cea7
Kamal <158833565+Kamal23052004@users.noreply.github.com>
2024-08-20 13:11:33 +0000

    Half Project Completed

---
+++ fourth-angular-project/Ecommerce/src/app/pages/admin/products/products.component.ts
@@ -0,0 +1,52 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../services/product/product.service';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent implements OnInit {
+  isSidePanelVisible: boolean= false;
+  productObj: any ={
+    "productId": 0,
+    "productSku": "",
+    "productName": "",
+    "productPrice": 0,
+    "productShortName": "",
+    "productDescription": "",
+    "createdDate": new Date(),
+    "deliveryTimeSpan": "",
+    "categoryId": 0,
+    "productImageUrl": ""
+  };
+  categoryList: any [] = [];
+
+  constructor(private productSrv: ProductService){
+
+  }
+  ngOnInit(): void {
+    this.getAllCategory();
+  }
+
+
+  getAllCategory() {
+    this.productSrv.getCategory().subscribe((res:any)=>{
+      this.categoryList = res.data;
+    })
+  }
+
+
+
+  openSidePanel(){
+    this.isSidePanelVisible = true;
+  }
+
+  closeSidePanel(){
+    this.isSidePanelVisible = true;
+  }
+}
